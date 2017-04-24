b1c40578747c828cea451f81ae65c83a672c5a7a
JDurand35 <julien.durand.35@hotmail.fr>
2017-04-24 12:12:10 +0200

    repar bug

--- FRONT_END/src/app/drive/drive.component.ts
+++ FRONT_END/src/app/drive/drive.component.ts
@@ -8,14 +8,19 @@ import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 export class DriveComponent implements OnInit {
   @Output() connect: EventEmitter<string> = new EventEmitter<string>();
   @Input() imageSource: string;
+  button: string = "Connecter";
 
   constructor() { }
 
   ngOnInit() {
   }
 
-
   synchro(drive: string) {
     this.connect.emit(drive);
+    if(this.button=="Connecter"){
+      this.button = "Déconnecter";
+    }else{
+      this.button = "Connecter";
+    }
   }
 }
---
+++ FRONT_END/src/app/files/files.component.ts
@@ -0,0 +1,137 @@
+import {Component, OnInit} from '@angular/core';
+import { File } from '../../model/file';
+import { Folder } from '../../model/folder';
+import {FileService} from "../../service/file.service";
+import {FolderService} from "../../service/folder.service";
+import { Element } from '../../model/element';
+
+
+@Component({
+  selector: 'app-files',
+  templateUrl: './files.component.html',
+  styleUrls: ['./files.component.css'],
+  providers: [FileService, FolderService]
+})
+
+export class FilesComponent implements OnInit{
+
+  constructor(private fileService : FileService, private  folderService: FolderService) {
+    this.paths.push("root");
+    this.concatPath();
+  }
+
+  ngOnInit() : void {
+    this.getFiles();
+    this.getFolders();
+  }
+
+  rightClicked : Element;
+  selectedElement: Element;
+  files : File[];
+  folders : Folder[];
+  copiedFile : Element;
+  pastedFile : Element;
+  paths : string[] = new Array();
+  path : string = '';
+  newName: string = '';
+  contextMenuPos: Object = {};
+
+  onSelect(element: Element): void {
+    this.selectedElement = element;
+  }
+
+  getFiles() : void {
+    this.files = this.fileService.getFiles();
+  }
+
+  getFolders() : void {
+    this.folders = this.folderService.getFolders();
+  }
+
+  detectRightMouseClick($event, element: Element) {
+    if ($event.which === 3) {
+      this.contextMenuPos = {'display': 'block', 'left': $event.clientX + 'px', 'top': $event.clientY + 'px'};
+      this.rightClicked = element;
+      return false;
+    }
+  }
+
+  closeContextMenu() {
+    this.contextMenuPos = {'display':'none'};
+    this.rightClicked = null;
+  }
+
+  onCopy(element: Element):void {
+    if(this.rightClicked != null){
+      this.copiedFile = this.rightClicked;
+      this.closeContextMenu();
+    }else{
+      this.copiedFile= element;
+    }
+  }
+
+  onPaste(): void {
+    if(this.copiedFile != null){
+      this.pastedFile = this.copiedFile.constructor();
+      this.pastedFile.name = this.copiedFile.name + " (copy)";
+      this.pastedFile.taille = this.copiedFile.taille;
+      this.pastedFile.isFolder = this.copiedFile.isFolder;
+      this.pastedFile.sharedList = this.copiedFile.sharedList;
+      if(this.pastedFile.isFolder){
+        this.pastedFile.key = this.folders.length;
+        this.folderService.addFolder(<Folder>this.pastedFile,this.folderService.getFolder(this.paths[this.paths.length-1]));
+      }else{
+        this.pastedFile.key = this.files.length;
+        this.fileService.addFile(this.pastedFile,this.folderService.getFolder(this.paths[this.paths.length-1]));
+      }
+    }
+  }
+
+  //Uniquement répertoire courant
+  createFile(folder: Folder){
+    let f : File = {key: this.files.length,name: this.newName,taille: 0, isFolder: false, sharedList: []};
+    this.fileService.addFile(f,folder);
+  }
+
+  //Uniquement répertoire courant
+  createFolder(folder: Folder){
+    let f : Folder = {key: this.folders.length, name: this.newName,taille: 0,files: [], isFolder: true, sharedList: []};
+    this.folderService.addFolder(f,folder);
+  }
+
+  onRemove(a : any):void {
+    if(this.rightClicked != null){
+      if(this.rightClicked.isFolder){
+        let folder = <Folder> this.rightClicked;
+        this.folderService.deleteFolder(folder);
+      }else{
+        this.fileService.deleteFile(<File> this.rightClicked);
+      }
+      this.rightClicked = null;
+      this.closeContextMenu();
+    }else{
+      this.selectedElement=null;
+    }
+  }
+
+  onOpen(folder : Folder){
+    this.paths.push(folder.name);
+    this.concatPath();
+  }
+
+
+  onComeBack(){
+    if(this.paths.length>1){
+      let f: Folder = this.folderService.getFolder(this.paths.pop());
+      this.concatPath();
+    }
+  }
+
+  concatPath(){
+    this.path = ''
+    for(let p of this.paths){
+      this.path+= " > " + p ;
+    }
+  }
+
+}
