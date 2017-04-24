ef9e8754b1edb74e05a7516f9db6ccf34d22b67a
Darzak <olivier.richard.peurichard@gmail.com>
2017-04-24 09:55:24 +0200

    navbar

--- FRONT_END/src/app/app.module.ts
+++ FRONT_END/src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule }   from '@angular/router';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FileDetailsComponent } from './file-details/file-details.component';
 import { LoginComponent } from './login/login.component';
@@ -11,6 +10,13 @@ import { ClickOutsideDirective } from './click-outside.directive';
 import { FilesComponent } from './files/files.component';
 import { DriveComponent } from './drive/drive.component';
 
+const appRoutes: Routes = [
+  {path: 'files', component: FilesComponent },
+  {path: 'login', component: LoginComponent },
+  {path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -25,16 +31,7 @@ import { DriveComponent } from './drive/drive.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-        {
-          path: 'files',
-          component: FilesComponent
-        },
-      {
-        path: '',
-        component: LoginComponent
-      }
-    ])
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
