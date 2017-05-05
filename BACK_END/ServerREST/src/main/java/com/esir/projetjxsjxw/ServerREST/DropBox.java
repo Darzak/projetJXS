3dccf3d55654d3f7e6dedaf742628f46630b0cbd
Bynaryman <i.f.lledoux@gmail.com>
2017-05-05 03:54:02 +0200

    delete

--- BACK_END/ServerREST/src/main/java/com/esir/projetjxsjxw/ServerREST/DropBox.java
+++ BACK_END/ServerREST/src/main/java/com/esir/projetjxsjxw/ServerREST/DropBox.java
@@ -132,22 +132,18 @@ public class DropBox {
 		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
 	}
 	
-	
 	@GET
 	@Path("/getStorage")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getStorage() throws JsonGenerationException, JsonMappingException, IOException {
 		WebResource webResource = client.resource("https://api.dropboxapi.com/2/users/get_space_usage");
 		ClientResponse clientResponse = webResource
-			.type(MediaType.APPLICATION_FORM_URLENCODED)
 			.header("Authorization", "Bearer " + token)
-			.header("Content-type", "application/json")
 			.post(ClientResponse.class);
-		String res = clientResponse.getEntity(String.class);	
+		String res = clientResponse.getEntity(String.class);
+		System.out.println(res);
 		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
 	}
-	
-	
 	
 	@GET
 	@Path("/createFile")
@@ -155,6 +151,8 @@ public class DropBox {
 	public Response createFiles(@QueryParam("path") String path) throws JsonGenerationException, JsonMappingException, IOException {
 		
 		WebResource webResource = client.resource("https://content.dropboxapi.com/2/files/upload");
+		
+		System.out.println(path);
 		
 		Map<String, Object> formDataToCreateFile = new HashMap<String, Object>();
 		formDataToCreateFile.put("path", path);
@@ -181,44 +179,6 @@ public class DropBox {
 	}
 	
 	@GET
-	@Path("/createFolder")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response createFolder(@QueryParam("path") String path) throws JsonGenerationException, JsonMappingException, IOException {	
-		WebResource webResource = client.resource("https://api.dropboxapi.com/2/files/create_folder");
-		Map<String, Object> formDataToCreateFolder = new HashMap<String, Object>();
-		formDataToCreateFolder.put("path", path);
-		formDataToCreateFolder.put("autorename", true);
-		String payload = new ObjectMapper().writeValueAsString(formDataToCreateFolder);				
-		ClientResponse clientResponse =
-				webResource
-				.header("Authorization", "Bearer " + token)
-				.entity(payload, MediaType.APPLICATION_JSON_TYPE)
-				.post(ClientResponse.class);
-		
-		String res = clientResponse.getEntity(String.class);
-		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
-	}
-	
-	@GET
-	@Path("/delete")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response delete(@QueryParam("path") String path) throws JsonGenerationException, JsonMappingException, IOException {	
-		WebResource webResource = client.resource("https://api.dropboxapi.com/2/files/delete");
-		Map<String, Object> formDataToDelete = new HashMap<String, Object>();
-		formDataToDelete.put("path", path);
-		String payload = new ObjectMapper().writeValueAsString(formDataToDelete);				
-		ClientResponse clientResponse =
-				webResource
-				.header("Authorization", "Bearer " + token)
-				.entity(payload, MediaType.APPLICATION_JSON_TYPE)
-				.post(ClientResponse.class);
-		
-		String res = clientResponse.getEntity(String.class);
-		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
-	}
-	
-	
-	@GET
 	@Path("/rename")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response renameFile( @QueryParam("input_path") String input_path,
@@ -230,6 +190,63 @@ public class DropBox {
 		formDataToChangeName.put("from_path", input_path);
 		formDataToChangeName.put("autorename", true);
 		formDataToChangeName.put("allow_shared_folder", false);
+		formDataToChangeName.put("to_path", new_path);
+		
+		String headerArgs = new ObjectMapper().writeValueAsString(formDataToChangeName);
+		
+		ClientResponse clientResponse =
+				webResource
+				.header("Authorization", "Bearer " + token)
+				.header("Content-Type", "application/json")
+				.type(MediaType.APPLICATION_JSON_TYPE)
+				.entity(headerArgs, MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class);
+		
+		String res = clientResponse.getEntity(String.class);
+				
+		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
+	}
+	
+	@GET
+	@Path("/createFolder")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response createFolder( @QueryParam("path") String input_path) throws JsonGenerationException, JsonMappingException, IOException {
+		
+		WebResource webResource = client.resource("https://api.dropboxapi.com/2/files/create_folder");
+				
+		Map<String, Object> formDataToCreateFolder = new HashMap<String, Object>();
+		formDataToCreateFolder.put("path", input_path);
+		formDataToCreateFolder.put("autorename", true);
+		
+		String headerArgs = new ObjectMapper().writeValueAsString(formDataToCreateFolder);
+		System.out.println("SERVER ---- CREATE FOLDER" + input_path);
+		ClientResponse clientResponse =
+				webResource
+				.header("Authorization", "Bearer " + token)
+				.header("Content-Type", "application/json")
+				.type(MediaType.APPLICATION_JSON_TYPE)
+				.entity(headerArgs, MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class);
+		
+		String res = clientResponse.getEntity(String.class);
+		
+		System.out.println("SERVER -- RESPONSE : "+res);
+				
+		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
+	}
+	
+	@GET
+	@Path("/paste")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response pasteFile( @QueryParam("input_path") String input_path,
+								@QueryParam("new_path")   String new_path ) throws JsonGenerationException, JsonMappingException, IOException {
+		
+		WebResource webResource = client.resource("https://api.dropboxapi.com/2/files/copy");
+				
+		Map<String, Object> formDataToChangeName = new HashMap<String, Object>();
+		formDataToChangeName.put("from_path", input_path);
+		formDataToChangeName.put("autorename", true);
+		formDataToChangeName.put("allow_shared_folder", true);
 		formDataToChangeName.put("to_path", new_path);
 		
 		String headerArgs = new ObjectMapper().writeValueAsString(formDataToChangeName);
@@ -284,6 +301,94 @@ public class DropBox {
 		String res = clientResponse.getEntity(String.class);
 		
 		System.out.println(res);
+						
+		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
+	}
+	
+	@GET 		
+	@Path("/delete")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response delete (@QueryParam("path")  String path ) throws Exception {
+	
+		System.out.println("SERVER ---- delete" +path);		
+		WebResource webResource = client.resource("https://api.dropboxapi.com/2/files/delete");
+		
+		Map<String, Object> formDataToDelete = new HashMap<String, Object>();
+		formDataToDelete.put("path", path);				
+		String headerArgs = new ObjectMapper().writeValueAsString(formDataToDelete);
+		
+		System.out.println("SERVER --- le path"+headerArgs);
+				
+		ClientResponse clientResponse =
+				webResource
+				.header("Authorization", "Bearer " + token)
+				.header("Content-Type", "application/json")
+				.type(MediaType.APPLICATION_JSON_TYPE)
+				.entity(headerArgs, MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class);
+		
+		String res = clientResponse.getEntity(String.class);
+		
+		System.out.print("SERVER ---- delete réponse" +res);
+						
+		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
+	}
+	
+	@GET
+	@Path("/shared_link")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response sharedLink (@QueryParam("path")  String path ) throws Exception {
+	
+		System.out.println("SERVER ---- shared_link" +path);		
+		WebResource webResource = client.resource("https://api.dropboxapi.com/2/sharing/create_shared_link");
+		
+		Map<String, Object> formDataToShare = new HashMap<String, Object>();
+		formDataToShare.put("path", path);
+		formDataToShare.put("short_url", false);
+		String headerArgs = new ObjectMapper().writeValueAsString(formDataToShare);
+		
+		System.out.println("SERVER --- le path"+headerArgs);
+				
+		ClientResponse clientResponse =
+				webResource
+				.header("Authorization", "Bearer " + token)
+				.header("Content-Type", "application/json")
+				.type(MediaType.APPLICATION_JSON_TYPE)
+				.entity(headerArgs, MediaType.APPLICATION_JSON)
+				.post(ClientResponse.class);
+		
+		String res = clientResponse.getEntity(String.class);
+		
+		System.out.print("SERVER ---- réponse" +res);
+						
+		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
+	}
+	
+	@GET
+	@Path("/downloadFiles")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response downloadFiles (@QueryParam("path")  String path ) throws Exception {
+	
+		System.out.println("DOWNLOAD " +path);		
+		WebResource webResource = client.resource("https://content.dropboxapi.com/2/files/download");
+		
+		Map<String, Object> formDataToDownload = new HashMap<String, Object>();
+		formDataToDownload.put("path", path);				
+		String headerArgs = new ObjectMapper().writeValueAsString(formDataToDownload);
+		
+		System.out.println(headerArgs);
+				
+		ClientResponse clientResponse =
+				webResource
+				.header("Authorization", "Bearer " + token)
+				.header("Dropbox-API-Arg", headerArgs)
+				.post(ClientResponse.class);
+		
+		System.out.println(clientResponse.getHeaders());
+		System.out.println(clientResponse.getStatus());
+		String res = clientResponse.getEntity(String.class);
+		
+		System.out.print("Download "+res);
 						
 		return Response.status(200).entity(res).header("Access-Control-Allow-Origin", "*").build();
 	}
